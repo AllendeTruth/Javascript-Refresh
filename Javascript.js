30cbb58ed2d4dc5985b61e307fe11e6a97524cbb
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 13:49:27 -0500

    order precedence

--- Javascript.js
+++ Javascript.js
@@ -15,22 +15,45 @@
 
 //Lecture: variables 2
 
-var name = "Nico";
-var age = 100;
+// var name = "Nico";
+// var age = 100;
 
-// console.log(name + age); //type coercion // returns Nico100
+// // console.log(name + age); //type coercion // returns Nico100
 
-// console.log(age + age); // 200
+// // console.log(age + age); // 200
 
-var job, isMarried;
-// console.log(job); //undefined
+// var job, isMarried;
+// // console.log(job); //undefined
 
-job = "teacher";
-isMarried = false;
+// job = "teacher";
+// isMarried = false;
 
-console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
-//variable mutation
-age = "thirty six";
-job = "hustler";
+// console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
+// //variable mutation
+// age = "thirty six";
+// job = "hustler";
 
-console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
+// console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
+
+
+// var lastName = prompt("What is the last name?");
+// console.log(lastName);
+
+// alert(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
+
+
+//Lecture: Operators
+var now = 2018
+var birthYear = now-100; //1918
+
+
+
+
+console.log(birthYear);
+//operator precedence * is higher priortity than -
+var ageMark = 30;
+var ageJohn = 30; 
+
+ageJohn = ageMark =(3+5)*4-6;
+
+console.log(ageJohn);
