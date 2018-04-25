a4a958773447388970d4b6363085e0af6ba598e1
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 13:23:09 -0500

    variables lecture 1 diff types of var

---
+++ Javascript.js
@@ -0,0 +1,17 @@
+// Lecture: Variables
+
+// var name = "Nico";
+// console.log(name);
+
+// var lastName = "Allende";
+// console.log(lastName);
+
+// var age = 100;
+// console.log(age);
+
+// var fullAge = true;
+// console.log(fullAge);
+
+
+//Lecture: variables 2
+
