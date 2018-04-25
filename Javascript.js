748093c1338ce1a4bc1d695d6381a35b4bba9fee
AllendeTruth <nandersizzle@gmail.com>
2018-04-25 13:32:52 -0500

    bookmark

--- Javascript.js
+++ Javascript.js
@@ -18,14 +18,19 @@
 var name = "Nico";
 var age = 100;
 
-console.log(name + age); //type coercion // returns Nico100
+// console.log(name + age); //type coercion // returns Nico100
 
-console.log(age + age); // 200
+// console.log(age + age); // 200
 
 var job, isMarried;
-console.log(job); //undefined
+// console.log(job); //undefined
 
 job = "teacher";
 isMarried = false;
+
+console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
+
+age = "thirty six";
+job = "hustler";
 
 console.log(name + " is " + age + " years old and a " + job + ". Is he married? " + isMarried +".");
